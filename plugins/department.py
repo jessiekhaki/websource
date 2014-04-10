c008f2a9dda3aadc442942eadae3d5d0fd2a6647
Barry Rowlingson <b.rowlingson@gmail.com>
2014-04-10 23:02:12 +0100

    events

--- plugins/department.py
+++ plugins/department.py
@@ -1,27 +1,48 @@
 from hyde.plugin import Plugin
+from jinja2 import environmentfilter, Environment
 
-print "imported"
+from datetime import datetime
+
+@environmentfilter
+def todateformat(env, value, verbose=False):
+    try:
+        when = datetime.strptime(value,"%Y-%m-%d %H:%M")
+    except:
+        try:
+            when = datetime.strptime(value,"%Y-%m-%d")
+        except:
+            raise ValueError
+   
+    return when.strftime("%d %B %Y")
+
+
+filters={
+    'todateformat': todateformat
+}
 
 class DepartmentPlugin(Plugin):
     
     def __init__(self,site):
         super(DepartmentPlugin, self).__init__(site)
-        #print "Dept init"
     
+    def template_loaded(self, template):
+        super(DepartmentPlugin, self).template_loaded(template)
+        self.template.env.filters.update(filters)
+
     def begin_site(self):
-         self.logger.info( "Department plugin" )
+        pass
 
     def site_complete(self):
-        self.logger.info("dept site complete")
+        pass
 
     def begin_node(self, node):
-        self.logger.info("dept begin node %s"  % str(node))
+        pass
 
     def node_complete(self, node):
         pass
     
     def begin_text_resource(self, resource, text):
-         print "begin %s %s " % (resource, text)
+        pass
 
     # def text_resource_complete(self, resource, text):
     #     print "complete ",resource, text)
