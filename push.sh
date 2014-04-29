a7ffd97ee391ab38fc5c9fc9f94949fa0d087fb4
Barry Rowlingson <b.rowlingson@gmail.com>
2014-04-29 21:09:13 +0100

    set git config settings

--- push.sh
+++ push.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "StrictHostKeyChecking no" >> ~/.ssh/config
 cd ..
+
+git config --global user.email "barry@rowlingson.com"
+git config --global user.name "Barry Rowlingson"
+
 git clone https://$GITKEY:x-oauth-basic@github.com/chicas-lancaster/chicas-lancaster.github.io.git
 cd websource
 hyde gen -r
