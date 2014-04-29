7283c0b186ba4aa70cd53390823b32b0b12fc6f4
Barry Rowlingson <b.rowlingson@gmail.com>
2014-04-29 21:22:30 +0100

    new key

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ cd ..
 git config --global user.email "barry@rowlingson.com"
 git config --global user.name "Barry Rowlingson"
 
-git clone https://$GITKEY:x-oauth-basic@github.com/chicas-lancaster/chicas-lancaster.github.io.git
+git clone https://$GITKEY:x-oauth-basic@github.com/chicas-lancaster/chicas-lancaster.github.io.git >/dev/null 2>&1
 cd websource
 hyde gen -r
 hyde publish
