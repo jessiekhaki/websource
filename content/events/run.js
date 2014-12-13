7a8114225df211c2e1f190e3a0987f94e5eacb92
Barry Rowlingson <b.rowlingson@gmail.com>
2014-12-13 20:27:28 +0000

    templates

--- content/events/run.js
+++ content/events/run.js
@@ -9,7 +9,9 @@ pad = function(v){
 
 templates = {
     eventList: $('<div class="eventlist">'),
-    eventItem: $('<div class="event"><h3 class="summary"><a href=""></a></h3><div class="start"/><div class="description"></div></div>')
+    eventItem: $('<div class="event"><h3 class="summary"><a href=""></a></h3><div class="start"><span class="when"/><span class="where"/></div><div class="description"></div></div>'),
+    upcomingBox: $('<div class="upcoming"><h4>Coming Next...</h4></div>'),
+    upcomingItem: $('<div class="event"><h6 class="when"/><h5 class="summary"><a href=""></a></h5><p class="description"></p><h6 class="where"/></div>')
 };
 
 getWhen = function(v){
@@ -33,15 +35,15 @@ now = new Date().toISOString();
 
 api="AIzaSyAB1Jd-Jf3U-R84BJzJAPTIYZZmM1sqtjs";
 
-getN = function(n, root){
+getN = function(n, root, wrapper, item){
     var r = root;
     var now = new Date().toISOString();
     var URL = "https://www.googleapis.com/calendar/v3/calendars/e8j7bjqajiblsstfcc59iimss0%40group.calendar.google.com/events?callback=?&maxResults="+n+"&timeMin="+now+"&orderBy=startTime&singleEvents=true&key="+api;
     $.getJSON(URL , function(json) {
 	var events = json.items;
-	var list = templates.eventList.clone();
+	var list = templates[wrapper].clone();
 	$.each(events, function(){
-	    var e = templates.eventItem.clone();
+	    var e = templates[item].clone();
 	    var when = getWhen(this);
 	    var where;
 	    if(this.location){
@@ -50,7 +52,8 @@ getN = function(n, root){
 		where = "";
 	    };
 	    e.find("a").text(this.summary).attr("href",this.htmlLink).end()
-		.find(".start").text(when+where).end()
+		.find(".when").text(when).end()
+		.find(".where").text(where).end()
 		.find(".description").text(this.description);
 	    list.append(e);
 	});
